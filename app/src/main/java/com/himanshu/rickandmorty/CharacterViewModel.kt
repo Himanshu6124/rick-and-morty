640c0f1da9eb117b4a9da13f1f18ca3982984c47
Himanshu <Himanshu6124@gmail.com>
2024-07-27 16:04:32 +0530

    added exception handling

--- app/src/main/java/com/himanshu/rickandmorty/CharacterViewModel.kt
+++ app/src/main/java/com/himanshu/rickandmorty/CharacterViewModel.kt
@@ -1,5 +1,6 @@
 package com.himanshu.rickandmorty
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,28 +9,65 @@ import com.himanshu.rickandmorty.model.CharacterResponse
 import kotlinx.coroutines.launch
 
 class CharacterViewModel(private val repository: CharacterRepository) : ViewModel() {
-
+    val TAG = "CharacterViewModel"
     private val _characters  = MutableLiveData<CharacterResponse>()
     val characters  : LiveData<CharacterResponse> = _characters
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean>  = _isError
+
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: LiveData<String?> = _errorMessage
+
     var currentPage = 1
 
     fun getCharacters(page :Int){
         viewModelScope.launch {
-            _isLoading.postValue(true)
-            val res = repository.getCharacters(page)
-            _isLoading.postValue(false)
-            _characters.postValue(res)
+            try {
+                _isLoading.postValue(true)
+                val res = repository.getCharacters(page)
+                _isLoading.postValue(false)
+                _characters.postValue(res)
+            }
+            catch (ex : Exception){
+                Log.e(TAG,"exception occurred in get characters")
+                _isError.postValue(true)
+                _isLoading.postValue(false)
+                _errorMessage.value = ex.message
+            }
         }
     }
 
     fun searchCharacters(name :String){
         viewModelScope.launch {
-            val res = repository.searchCharacters(name)
-            _characters.postValue(res)
+            try {
+                _isLoading.postValue(true)
+                val res = repository.searchCharacters(name)
+                _isLoading.postValue(false)
+                _characters.postValue(res)
+            }
+            catch (ex : Exception){
+                Log.e(TAG,"exception occurred in search characters")
+                _isError.postValue(true)
+                _isLoading.postValue(false)
+                _errorMessage.value = ex.message
+            }
         }
     }
+
+
+//    private fun onError(errorMessage: String?) {
+//
+//        val message = if (errorMessage.isNullOrBlank() or errorMessage.isNullOrEmpty()) "Unknown Error"
+//        else errorMessage
+//
+//        errorMessage = StringBuilder("ERROR: ")
+//            .append("$message some data may not displayed properly").toString()
+//
+//        _isError.value = true
+//        _isLoading.value = false
+//    }
 }
--- app/src/main/java/com/himanshu/rickandmorty/MainFragment.kt
+++ app/src/main/java/com/himanshu/rickandmorty/MainFragment.kt
@@ -2,15 +2,16 @@ package com.himanshu.rickandmorty
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.himanshu.rickandmorty.databinding.FragmentMainBinding
 
 
@@ -39,26 +40,13 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(layoutInflater)
-
-        characterViewModel.characters.observe(viewLifecycleOwner) {
-            Log.i(TAG, "Response is $it")
-            val adapter = CharacterAdapter(it.results){character ->
-                Log.i(TAG,"character clicked ${character.name}")
-                val action = MainFragmentDirections.actionMainFragmentToDetailFragment(character)
-                findNavController().navigate(action)
-            }
-            binding.recyclerView.layoutManager = LinearLayoutManager(activity,
-                RecyclerView.VERTICAL,false)
-            binding.recyclerView.adapter = adapter
-
-        }
-
-        characterViewModel.isLoading.observe(viewLifecycleOwner){isLoading ->
-            if(isLoading) binding.loader.visibility = View.VISIBLE
-            else binding.loader.visibility = View.GONE
-        }
-
         characterViewModel.getCharacters(characterViewModel.currentPage)
+        addSubscribers()
+        addListeners()
+        return binding.root
+    }
+
+    private fun addListeners() {
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -77,13 +65,39 @@ class MainFragment : Fragment() {
         }
 
         binding.getPrevList.setOnClickListener {
-            if(characterViewModel.currentPage > 1){
+            if (characterViewModel.currentPage > 1) {
                 characterViewModel.currentPage--
                 characterViewModel.getCharacters(characterViewModel.currentPage)
             }
         }
-
-        return binding.root
     }
 
+
+    private fun addSubscribers() {
+        characterViewModel.characters.observe(viewLifecycleOwner) {
+            Log.i(TAG, "Response is $it")
+            val adapter = CharacterAdapter(it.results) { character ->
+                Log.i(TAG, "character clicked ${character.name}")
+                val action = MainFragmentDirections.actionMainFragmentToDetailFragment(character)
+                findNavController().navigate(action)
+            }
+            binding.recyclerView.layoutManager = LinearLayoutManager(
+                activity,
+                RecyclerView.VERTICAL, false
+            )
+            binding.recyclerView.adapter = adapter
+
+        }
+
+        characterViewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            if (isLoading) binding.loader.visibility = View.VISIBLE
+            else binding.loader.visibility = View.GONE
+        }
+
+        characterViewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
+            errorMessage?.let {
+                Snackbar.make(requireView(), it, Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
 }
